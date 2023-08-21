b956189965832c967de52bea0d43370b30694957
00013540 <wiut00013540@gmail.com>
2023-08-21 19:48:00 +0500

    Add header

--- components/layouts/TheHeader.vue
+++ components/layouts/TheHeader.vue
@@ -1,3 +1,35 @@
 <template>
-  <div>Header</div>
+  <div class="d-flex justify-between align-center header">
+    <div class="d-flex align-center">
+      <VIcon class="mr-8" icon="logo" size="35" />
+      <span class="header__logo__text">Hyperion</span>
+    </div>
+    <div
+      class="d-flex flex-column justify-center align-center header__menu"
+      @click="setIsMenuOpen"
+    >
+      <span class="header__menu__left" :class="{ open: isMenuOpen }" />
+      <span class="header__menu__right" :class="{ open: isMenuOpen }" />
+    </div>
+  </div>
 </template>
+<script>
+import VIcon from '~/components/ui/VIcon'
+export default {
+  components: { VIcon },
+  data() {
+    return {
+      isMenuOpen: false,
+    }
+  },
+  methods: {
+    setIsMenuOpen() {
+      this.isMenuOpen = !this.isMenuOpen
+    },
+  },
+}
+</script>
+
+<style lang="scss" scoped>
+@import '../../assets/styles/components/the-header';
+</style>
---
+++ components/ui/VIcon.vue
@@ -0,0 +1,35 @@
+<template>
+  <component
+    :is="importedIcon"
+    class="svg-icon"
+    v-bind="$attrs"
+    :style="styles"
+    v-on="$listeners"
+  />
+</template>
+<script>
+export default {
+  name: 'VIcon',
+  props: {
+    icon: {
+      type: String,
+      required: true,
+    },
+    size: {
+      type: [Number, String],
+      default: 24,
+    },
+  },
+  computed: {
+    importedIcon() {
+      return () => import(`@/assets/icons/${this.icon}.svg?inline`)
+    },
+    styles() {
+      return {
+        width: this.size + 'px',
+        height: this.size + 'px',
+      }
+    },
+  },
+}
+</script>
