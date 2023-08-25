a358693d896495bc3b1ac0c0bb9732a4988320e6
00013540 <wiut00013540@gmail.com>
2023-08-25 14:07:38 +0500

    HomeOffer

--- data/home-offer.js
+++ data/home-offer.js
@@ -1,27 +1,40 @@
 const data = [
   {
     id: 1,
-    image: '/car-1.webp',
+    image: '/car-5.webp',
     title: 'DOOR-TO-DOOR TRANSPORT',
     description:
       'Hyperion Auto Logistics is known for safe and secure door-to-door auto transportation',
   },
   {
     id: 2,
-    image: '/car-2.webp',
+    image: '/car-5.webp',
     title: 'OPEN CARRIER TRANSPORT',
     description:
       'Hyperion Auto Logistics has years of open car shipping experience',
   },
   {
     id: 3,
-    image: '/car-3.webp',
+    image: '/car-5.webp',
     title: 'ENCLOSED TRANSPORT',
     description: 'Enclosed auto transport protects your car during shipping',
   },
   {
     id: 4,
-    image: '/car-4.webp',
+    image: '/car-5.webp',
+    title: 'MOTORCYCLE SHIPPING',
+    description:
+      'Hyperion Auto Logistics ships motorcycles of all types across all 50 states',
+  },
+  {
+    id: 5,
+    image: '/car-5.webp',
+    title: 'ENCLOSED TRANSPORT',
+    description: 'Enclosed auto transport protects your car during shipping',
+  },
+  {
+    id: 6,
+    image: '/car-5.webp',
     title: 'MOTORCYCLE SHIPPING',
     description:
       'Hyperion Auto Logistics ships motorcycles of all types across all 50 states',
