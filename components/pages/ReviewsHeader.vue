defb11c273a30758dc557ddcfa7c0528b7f2b6d8
00013540 <wiut00013540@gmail.com>
2023-09-04 10:50:51 +0500

    Add ReviewsHeader.vue

---
+++ components/pages/ReviewsHeader.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="reviews__wrapper">
+    <div class="reviews__image__box">
+      <img class="reviews__image" src="/hero.webp" alt="image" />
+    </div>
+    <div class="reviews__stars">
+      <VIcon icon="star" size="22" />
+      <VIcon icon="star" size="22" />
+      <VIcon icon="star" size="22" />
+      <VIcon icon="star" size="22" />
+      <VIcon icon="star" size="22" />
+    </div>
+    <div class="reviews_content">
+      <h1>Get your free quote now</h1>
+      <p>
+        Have your car shipped to and from any state - safely, fast, with no risk
+        or damage.
+      </p>
+      <button>Read more</button>
+    </div>
+  </div>
+</template>
+
+<script>
+import VIcon from '~/components/ui/VIcon'
+export default {
+  name: 'ReviewsHeader',
+  components: { VIcon },
+}
+</script>
+
+<style lang="scss" scoped>
+@import '../../assets/styles/pages/reviews-header';
+</style>
---
+++ pages/reviews.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    <ReviewsHeader />
+  </div>
+</template>
+
+<script>
+import ReviewsHeader from '~/components/pages/ReviewsHeader'
+export default {
+  name: 'ReviewsPage',
+  components: { ReviewsHeader },
+}
+</script>
