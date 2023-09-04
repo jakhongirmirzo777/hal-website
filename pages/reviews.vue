11dec89f72096ca5aaf3f29f6355efee76533209
00013540 <wiut00013540@gmail.com>
2023-09-04 11:29:39 +0500

    Add ReviewsBlogs.vue

--- pages/reviews.vue
+++ pages/reviews.vue
@@ -4,6 +4,8 @@
     <HeroReviews />
     <ReviewsSlider />
     <ReviewsQuotes />
+    <HeroForm />
+    <HomeBlog style="margin-top: 100px" />
   </div>
 </template>
 
@@ -12,8 +14,17 @@ import ReviewsHeader from '~/components/pages/ReviewsHeader'
 import HeroReviews from '~/components/pages/HeroReviews'
 import ReviewsSlider from '~/components/pages/RreviewsSlider'
 import ReviewsQuotes from '~/components/pages/ReviewsQuotes'
+import HeroForm from '~/components/pages/HeroForm'
+import HomeBlog from '~/components/pages/HomeBlog'
 export default {
   name: 'ReviewsPage',
-  components: { ReviewsQuotes, ReviewsSlider, HeroReviews, ReviewsHeader },
+  components: {
+    HomeBlog,
+    HeroForm,
+    ReviewsQuotes,
+    ReviewsSlider,
+    HeroReviews,
+    ReviewsHeader,
+  },
 }
 </script>
