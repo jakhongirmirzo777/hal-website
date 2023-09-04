f328b8125e454cfb7ed64c2f3e0eb24740849380
00013540 <wiut00013540@gmail.com>
2023-09-04 11:38:32 +0500

    Add ReviewsHeader.vue responsiveness

--- components/pages/ReviewsHeader.vue
+++ components/pages/ReviewsHeader.vue
@@ -3,20 +3,22 @@
     <div class="reviews__image__box">
       <img class="reviews__image" src="/hero.webp" alt="image" />
     </div>
-    <div class="reviews__stars">
-      <VIcon icon="star" size="22" />
-      <VIcon icon="star" size="22" />
-      <VIcon icon="star" size="22" />
-      <VIcon icon="star" size="22" />
-      <VIcon icon="star" size="22" />
-    </div>
-    <div class="reviews_content">
-      <h1>Get your free quote now</h1>
-      <p>
-        Have your car shipped to and from any state - safely, fast, with no risk
-        or damage.
-      </p>
-      <button>Read more</button>
+    <div class="reviews__data">
+      <div class="reviews__stars">
+        <VIcon icon="star" size="22" />
+        <VIcon icon="star" size="22" />
+        <VIcon icon="star" size="22" />
+        <VIcon icon="star" size="22" />
+        <VIcon icon="star" size="22" />
+      </div>
+      <div class="reviews__content">
+        <h1>Get your free quote now</h1>
+        <p>
+          Have your car shipped to and from any state - safely, fast, with no
+          risk or damage.
+        </p>
+        <button>Read more</button>
+      </div>
     </div>
   </div>
 </template>
