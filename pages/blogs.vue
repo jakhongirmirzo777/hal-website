a3993177fa755abbf9c6c6765e296ef0bff01148
00013540 <wiut00013540@gmail.com>
2023-09-04 21:16:58 +0500

    Add blogspage is ready

--- pages/blogs.vue
+++ pages/blogs.vue
@@ -1,13 +1,17 @@
 <template>
   <div>
     <BlogsCard />
+    <HeroReviews />
+    <HeroForm style="margin-bottom: 150px" />
   </div>
 </template>
 
 <script>
 import BlogsCard from '~/components/pages/BlogsCard'
+import HeroReviews from '~/components/pages/HeroReviews'
+import HeroForm from '~/components/pages/HeroForm'
 export default {
   name: 'BlogsPage',
-  components: { BlogsCard },
+  components: { HeroForm, HeroReviews, BlogsCard },
 }
 </script>
