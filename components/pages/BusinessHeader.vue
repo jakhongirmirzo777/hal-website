a2c025865d1f932b1e6578855ea1dba590bd1f12
00013540 <wiut00013540@gmail.com>
2023-09-05 20:46:14 +0500

    Add BusinessPage is ready

---
+++ components/pages/BusinessHeader.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="business__wrapper">
+    <div class="business__title">
+      <h1>Get your free quote now</h1>
+      <p>
+        Have your car shipped to and from any state - safely, fast, with no risk
+        or damage.
+      </p>
+    </div>
+    <button>Start your search</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'BusinessHeader',
+}
+</script>
+
+<style lang="scss" scoped>
+@import '../../assets/styles/pages/business-header';
+</style>
---
+++ pages/business.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    <BusinessHeader />
+  </div>
+</template>
+
+<script>
+import BusinessHeader from '~/components/pages/BusinessHeader'
+export default {
+  name: 'WorksPage',
+  components: { BusinessHeader },
+}
+</script>
