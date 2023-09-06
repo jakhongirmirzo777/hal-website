688a36c0a8dae3487370011934906d07b311439b
00013540 <wiut00013540@gmail.com>
2023-09-06 17:38:14 +0500

    Add BusinessTransparency.vue is ready

---
+++ components/pages/BusinessTransparency.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="transparency__wrapper">
+    <img class="mobile" src="/car-12.webp" alt="image" />
+    <img class="desktop" src="/car-13.webp" alt="image" />
+    <div class="transparency__content">
+      <h2>Full transparency in prices</h2>
+      <p>
+        Have your car shipped to and from any state - safely, fast, with no risk
+        or damage.
+      </p>
+      <button>Start your search</button>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'BusinessTransparency',
+}
+</script>
+
+<style lang="scss" scoped>
+@import '../../assets/styles/pages/business-transparency';
+</style>
--- pages/business.vue
+++ pages/business.vue
@@ -2,14 +2,16 @@
   <div>
     <BusinessHeader />
     <BusinessBenefits />
+    <BusinessTransparency />
   </div>
 </template>
 
 <script>
 import BusinessHeader from '~/components/pages/BusinessHeader'
 import BusinessBenefits from '~/components/pages/BusinessBenefits'
+import BusinessTransparency from '~/components/pages/BusinessTransparency'
 export default {
   name: 'WorksPage',
-  components: { BusinessBenefits, BusinessHeader },
+  components: { BusinessTransparency, BusinessBenefits, BusinessHeader },
 }
 </script>
