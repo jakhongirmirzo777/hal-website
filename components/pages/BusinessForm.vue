d5f62ea2d6e567af43cb636cd7ce509479c15158
00013540 <wiut00013540@gmail.com>
2023-09-06 18:39:32 +0500

    Add BusinessPrices.vue is ready

---
+++ components/pages/BusinessForm.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="business-form__wrapper">
+    <VIcon
+      class="business-form__icon"
+      icon="map-black"
+      height="196"
+      width="322"
+    />
+    <FillForm class="business-form__form" />
+  </div>
+</template>
+
+<script>
+import VIcon from '~/components/ui/VIcon'
+import FillForm from '~/components/pages/FillForm'
+export default {
+  name: 'BusinessForm',
+  components: { FillForm, VIcon },
+}
+</script>
+
+<style lang="scss">
+@import '../../assets/styles/pages/business-form';
+</style>
--- pages/business.vue
+++ pages/business.vue
@@ -4,6 +4,12 @@
     <BusinessBenefits />
     <BusinessTransparency />
     <BusinessPrices />
+    <BusinessForm />
+    <div style="margin-bottom: -120px">
+      <HeroReviews />
+    </div>
+    <HomeFaq />
+    <HomeBlog />
   </div>
 </template>
 
@@ -12,9 +18,17 @@ import BusinessHeader from '~/components/pages/BusinessHeader'
 import BusinessBenefits from '~/components/pages/BusinessBenefits'
 import BusinessTransparency from '~/components/pages/BusinessTransparency'
 import BusinessPrices from '~/components/pages/BusinessPrices'
+import BusinessForm from '~/components/pages/BusinessForm'
+import HeroReviews from '~/components/pages/HeroReviews'
+import HomeFaq from '~/components/pages/HomeFaq'
+import HomeBlog from '~/components/pages/HomeBlog'
 export default {
   name: 'WorksPage',
   components: {
+    HomeBlog,
+    HomeFaq,
+    HeroReviews,
+    BusinessForm,
     BusinessPrices,
     BusinessTransparency,
     BusinessBenefits,
