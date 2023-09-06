657b6c80c616887d504d0ae4f0df322361115a57
00013540 <wiut00013540@gmail.com>
2023-09-06 16:58:24 +0500

    Add BusinessBenefits is ready

---
+++ components/pages/BusinessBenefits.vue
@@ -0,0 +1,64 @@
+<template>
+  <div class="benefits__wrapper">
+    <div class="benefits__title">
+      <h2>Full transparency in prices</h2>
+      <p>
+        Have your car shipped to and from any state - safely, fast, with no risk
+        or damage.
+      </p>
+    </div>
+    <div class="benefits__cards">
+      <div class="benefits__box">
+        <div class="benefits__card">
+          <div class="icon">
+            <VIcon size="45" icon="check-black" />
+          </div>
+          <h3>Get a Quote and Secure Your Contract</h3>
+          <p>
+            Our seamless process allows you to receive an accurate service quote
+            and sign the contract online. Minimal effort, maximum transparency.
+          </p>
+        </div>
+        <div class="benefits__line" />
+      </div>
+      <div class="benefits__box">
+        <div class="benefits__card">
+          <div class="icon">
+            <VIcon size="45" icon="date-black" />
+          </div>
+          <h3>Get a Quote and Secure Your Contract</h3>
+          <p>
+            Our seamless process allows you to receive an accurate service quote
+            and sign the contract online. Minimal effort, maximum transparency.
+          </p>
+        </div>
+        <div class="benefits__line" />
+      </div>
+      <div class="benefits__box">
+        <div class="benefits__card">
+          <div class="icon">
+            <VIcon size="45" icon="truck-black" />
+          </div>
+          <h3>Get a Quote and Secure Your Contract</h3>
+          <p>
+            Our seamless process allows you to receive an accurate service quote
+            and sign the contract online. Minimal effort, maximum transparency.
+          </p>
+        </div>
+        <div class="benefits__line" />
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import VIcon from '~/components/ui/VIcon'
+export default {
+  name: 'BusinessBenefits',
+  components: { VIcon },
+}
+</script>
+
+<style lang="scss" scoped>
+@import '../../assets/styles/pages/business-benefits';
+</style>
--- pages/business.vue
+++ pages/business.vue
@@ -1,13 +1,15 @@
 <template>
   <div>
     <BusinessHeader />
+    <BusinessBenefits />
   </div>
 </template>
 
 <script>
 import BusinessHeader from '~/components/pages/BusinessHeader'
+import BusinessBenefits from '~/components/pages/BusinessBenefits'
 export default {
   name: 'WorksPage',
-  components: { BusinessHeader },
+  components: { BusinessBenefits, BusinessHeader },
 }
 </script>
