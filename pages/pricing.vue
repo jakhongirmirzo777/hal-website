bdd9b8f5c3f26bf507853355966e5c1bb80d371f
00013540 <wiut00013540@gmail.com>
2023-09-07 21:31:40 +0500

    Pricing page is ready

---
+++ pages/pricing.vue
@@ -0,0 +1,33 @@
+<template>
+  <div>
+    <WorksDiscover />
+    <ReasonsPrices />
+    <WorksStepOne />
+    <HeroReviews />
+    <HomeFaq style="margin-top: -160px" />
+    <HeroForm style="margin-bottom: 150px" />
+    <HomeBlog />
+  </div>
+</template>
+
+<script>
+import WorksDiscover from '~/components/pages/WorksDiscover'
+import ReasonsPrices from '~/components/pages/ReasonsPrices'
+import WorksStepOne from '~/components/pages/WorksStepOne'
+import HeroReviews from '~/components/pages/HeroReviews'
+import HomeFaq from '~/components/pages/HomeFaq'
+import HeroForm from '~/components/pages/HeroForm'
+import HomeBlog from '~/components/pages/HomeBlog'
+export default {
+  name: 'PricingPage',
+  components: {
+    HomeBlog,
+    HeroForm,
+    HomeFaq,
+    HeroReviews,
+    WorksStepOne,
+    ReasonsPrices,
+    WorksDiscover,
+  },
+}
+</script>
