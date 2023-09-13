bb476c85818da662e5717fcb8521042c5216455c
00013540 <wiut00013540@gmail.com>
2023-09-13 20:25:47 +0500

    Fixed the map

--- components/pages/BusinessForm.vue
+++ components/pages/BusinessForm.vue
@@ -1,11 +1,13 @@
 <template>
   <div class="business-form__wrapper">
     <VIcon
+      id="map-black"
       class="business-form__icon"
       icon="map-black"
       height="196"
       width="322"
     />
+    <div id="tooltip-map-black"></div>
     <FillForm class="business-form__form" />
   </div>
 </template>
@@ -13,9 +15,40 @@
 <script>
 import VIcon from '~/components/ui/VIcon'
 import FillForm from '~/components/pages/FillForm'
+import states from '~/utils/states'
 export default {
   name: 'BusinessForm',
   components: { FillForm, VIcon },
+  mounted() {
+    setTimeout(() => {
+      const mapBlack = document.getElementById('map-black')
+      mapBlack.addEventListener('mouseover', this.placeTooltip)
+      mapBlack.addEventListener('mouseleave', this.removeTooltip)
+    }, 1000)
+  },
+  destroyed() {
+    const mapBlack = document.getElementById('map-black')
+    mapBlack.removeEventListener('mouseover', this.placeTooltip)
+    mapBlack.removeEventListener('mouseleave', this.removeTooltip)
+  },
+  methods: {
+    placeTooltip(e) {
+      const text = e.target.dataset.tooltip
+      const tooltip = document.getElementById('tooltip-map-black')
+      if (text) {
+        tooltip.textContent = states[text]
+        tooltip.style.left = e.clientX + 'px'
+        tooltip.style.top = e.clientY - 20 + 'px'
+        tooltip.style.opacity = '1'
+      } else {
+        tooltip.style.opacity = '0'
+      }
+    },
+    removeTooltip() {
+      const tooltip = document.getElementById('tooltip-map-black')
+      tooltip.style.opacity = '0'
+    },
+  },
 }
 </script>
 
